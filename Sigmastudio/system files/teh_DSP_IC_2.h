248ebc4033f6726130534f6266bebd22d94b3d3b
Matti Kullberg <matti.kullberg@iki.fi>
2021-10-22 11:16:24 +0300

    Initial

---
+++ Sigmastudio/system files/teh_DSP_IC_2.h
@@ -0,0 +1,38 @@
+/*
+ * File:           Z:\Sigmastudio teh DSP\system files\teh_DSP_IC_2.h
+ *
+ * Created:        Friday, October 22, 2021 11:07:33 AM
+ * Description:    teh_DSP:IC 2 program data.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2021 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __TEH_DSP_IC_2_H__
+#define __TEH_DSP_IC_2_H__
+
+#include "SigmaStudioFW.h"
+#include "teh_DSP_IC_2_REG.h"
+
+#define DEVICE_ARCHITECTURE_IC_2                  "E2Prom"
+#define DEVICE_ADDR_IC_2                          0xA0
+
+
+
+/*
+ * Default Download
+ */
+#define DEFAULT_DOWNLOAD_SIZE_IC_2 0
+
+void default_download_IC_2() {
+}
+
+#endif
---
+++ Sigmastudio/system files/teh_DSP_IC_2_REG.h
@@ -0,0 +1,23 @@
+/*
+ * File:           Z:\Sigmastudio teh DSP\system files\teh_DSP_IC_2_REG.h
+ *
+ * Created:        Friday, October 22, 2021 11:07:33 AM
+ * Description:    teh_DSP:IC 2 control register definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2021 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __TEH_DSP_IC_2_REG_H__
+#define __TEH_DSP_IC_2_REG_H__
+
+
+#endif
